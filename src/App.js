ac649b4bc92a7b1aa537ada35ada097b56595d35
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-02-19 21:04:18 -0500

    Add styling to page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Form, Jumbotron, Button, InputGroup, FormControl, ListGroup, Container } from 'react-bootstrap';
 import { v1 as uudidv4 } from 'uuid';
 import './App.css';
 
@@ -37,28 +38,39 @@ const App = () => {
       const newTodos = localStorage.getItem("Todo")
       setTodos(JSON.parse([...todos, newTodos]))
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="App">
-      <div className="container">
-        <form onSubmit={addTodo}>
-          <input type="text"
-            autoFocus
-            placeholder="Add a task..."
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button type="submit">Add</button>
-        </form>
-        {todos.map((todo) => (
-          <div key={todo.id} className="todo">
-            <p>{todo.text}</p>
-            <i onClick={() => removeTodo(todo.id)} className="fas fa-trash-alt"></i>
-          </div>
-        ))}
-      </div>
+      <Jumbotron className="jumbotron" fluid>WHAT'S ON YOUR TODO LIST?</Jumbotron>
+      <Container>
+        <div className="list-group">
+          <Form onSubmit={addTodo}>
+            <InputGroup size="default" className="mb-3">
+
+            
+
+              <FormControl type="text"
+                autoFocus
+                placeholder="Add a task..."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <InputGroup.Append>
+                <Button size="defatul" variant="info" type="submit">Add</Button>
+              </InputGroup.Append>
+            </InputGroup>
+
+          </Form>
+          {todos.map((todo) => (
+            <ListGroup.Item key={todo.id} className="todo">
+              <p>{todo.text}</p>
+              <i onClick={() => removeTodo(todo.id)} className="fas fa-trash-alt"></i>
+            </ListGroup.Item>
+          ))}
+        </div>
+      </Container>
     </div>
   );
 }
